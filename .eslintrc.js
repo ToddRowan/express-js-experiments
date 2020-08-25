a4321645570c14a2ac80d337def6bd95643a690f
Todd Rowan <todd.rowan@gmail.com>
2020-08-24 22:04:19 -0700

    Fixed Eslint config file after eslint didn't like it.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
-	"env": {
-		"es6": true,
-		"node": true
-	},
-	"extends": "eslint:recommended",
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
-	},
-	"parserOptions": {
-		"ecmaVersion": 2018,
-		"sourceType": "module"
-	},
-	"rules": {
-		"indent": [
-			"error",
-			"2"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"single"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
-		"no-unused-vars": [
-			"warn", 
-			{ 
-				"vars": "all", 
-				"args": "all", 
-				"ignoreRestSiblings": false
-			}
-		]
-	}
+  'env': {
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-unused-vars': [
+      'warn', 
+      { 
+        'vars': 'all', 
+        'args': 'all', 
+        'ignoreRestSiblings': false
+      }
+    ]
+  }
 };
