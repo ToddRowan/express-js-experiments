d2b2526f8a9dda42441d854a6dd1a22851ee7652
Todd Rowan <todd.rowan@gmail.com>
2020-11-27 16:05:42 -0800

    Added some tests.

---
+++ test/db/blogPostDbActions.test.js
@@ -0,0 +1,8 @@
+const bpda = require('../../lib/db/mysql/blogPostDbActions.js');
+
+test('We get an insert to complete.', () => {
+  expect.assertions(1);
+  let actions = new bpda(null);
+  // Use this syntax for testing methods that return promises.
+  return expect(actions.create()).resolves.toBeGreaterThan(0);
+});
---
+++ test/db/blogPostModel.test.js
@@ -0,0 +1,31 @@
+const bpm = require('../../lib/db/blogPostModel.js');
+
+test('Empty object to have undefined properties.', () => {
+  let model = new bpm();
+  expect(model.title).toBe(undefined);
+});
+
+test('Just a title, no author or content.', () => {
+  let model = new bpm({title:'title'});
+  expect(model.title).toBe('title');
+  expect(model.author).toBe(undefined);
+  expect(model.content).toBe(undefined);
+});
+
+test('All properties set.', () => {
+  let model = new bpm({title:'title', author:'author', content: 'content', 'id':1});
+  expect(model.title).toBe('title');
+  expect(model.author).toBe('author');
+  expect(model.content).toBe('content');
+  expect(model.id).toBe(1);
+});
+
+test('Empty object to have false for publish status.', () => {
+  let model = new bpm();
+  expect(model.status).toBe(false);
+});
+
+test('Empty object to have true for comment status.', () => {
+  let model = new bpm();
+  expect(model.commentStatus).toBe(true);
+});
