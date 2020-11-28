e8d259dfb145605ab730ced104dfa40a2abfac13
Todd Rowan <todd.rowan@gmail.com>
2020-11-28 13:59:22 -0800

    Changed db field names to match property names in model class.

--- utils/schema/sql/db.sql
+++ utils/schema/sql/db.sql
@@ -2,14 +2,14 @@ CREATE TABLE posts (
         id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(256) NOT NULL,
         author INT UNSIGNED NOT NULL,
-        created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-        modified_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+        createdDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+        modifiedDate DATETIME DEFAULT CURRENT_TIMESTAMP,
         content MEDIUMTEXT,
         excerpt TEXT,
         name VARCHAR(255) NOT NULL,
         guid VARCHAR(64) NOT NULL,
         status TINYINT UNSIGNED DEFAULT 0,
-        comment_status BIT DEFAULT 1,
+        commentStatus BIT DEFAULT 1,
         UNIQUE KEY unique_guid (guid)
     )
 ENGINE=InnoDB CHARACTER SET utf8;
