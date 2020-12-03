27faaffebc8c03dda45ea1d850cb2ba94a062774
Todd Rowan <todd.rowan@gmail.com>
2020-12-02 22:29:27 -0800

    More of the same.

--- test/db/blogPostDbActions.test.js
+++ test/db/blogPostDbActions.test.js
@@ -1,8 +1,11 @@
 const bpda = require('../../lib/db/mysql/actions/blogPostDbActions.js');
+const bpm  = require('../../lib/db/blogPostModel');
 
 test('We get an insert to complete.', () => {
+  // Set this to capture a failure of a promise to resolve.
   expect.assertions(1);
-  let actions = new bpda(null);
+  let bpmInst = new bpm({title: 'title', author: 7, name: 'name', guid: Date.now().toString()});
+  let actions = new bpda();
   // Use this syntax for testing methods that return promises.
-  return expect(actions.create()).resolves.toBeGreaterThan(0);
+  return expect(actions.create(bpmInst)).resolves.toBeGreaterThan(0);
 });
